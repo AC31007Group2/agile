b40920ab63cd705c70ba6c87b3df46a16fa209e1
dogmaan <dogmaan@gmail.com>
2014-02-08 13:54:22 +0000

    started work on options menu
    the options menu will be used to select the mock data feed

--- AgileProject/src/com/StockTake/AgileProjectActivity.java
+++ AgileProject/src/com/StockTake/AgileProjectActivity.java
@@ -4,12 +4,34 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class AgileProjectActivity extends TabActivity {
 	
 	StockManager myStockmanager = new StockManager();
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId()) {
+		case R.id.live_data:
+			return true;
+		case R.id.mock_data:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 	public void onCreate(Bundle savedInstanceState) {
 		// Seems that this is the main entry point in this project.
 	    super.onCreate(savedInstanceState);

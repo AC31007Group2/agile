16aa97a2fe5dc34f51e852e98ff5aaff36295567
Todd Harding <tharding@dundee.ac.uk>
2014-03-04 12:10:38 +0000

    Started Migrating to ActionBar to remove depracation warning

--- AgileProject/src/com/StockTake/AgileProjectActivity.java
+++ AgileProject/src/com/StockTake/AgileProjectActivity.java
@@ -1,17 +1,21 @@
 package com.StockTake;
 
 //import android.app.Activity;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TabHost;
 
-public class AgileProjectActivity extends TabActivity {
-	
+public class AgileProjectActivity extends FragmentActivity {
+
 	StockManager myStockmanager = new StockManager();
 	
 	@Override
@@ -25,20 +29,48 @@ public class AgileProjectActivity extends TabActivity {
 	{
 		switch (item.getItemId()) {
 		case R.id.settings_menu:
-			Intent intent = new Intent(this, SettingsActivity.class);
-			startActivity(intent);
-			return true;
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        case R.id.summary_fragment_alphabetical:
+            FragmentManager fragmentManager = getFragmentManager();
+            SummaryFragment fragment = new SummaryFragment();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
 
-	public void onCreate(Bundle savedInstanceState) 
+    public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
-	    Resources res = getResources(); // Resource object to get Drawables
+	    setContentView(R.layout.main_layout);
 
+        if(findViewById(R.id.fragment_container) != null)
+        {
+            if(savedInstanceState != null)
+            {
+                return;
+            }
+            FragmentManager fragmentManager = getFragmentManager();
+            SummaryFragment summaryFragment = new SummaryFragment();
+
+            SummaryFragment.Mode mode = SummaryFragment.Mode.ALPHABETICAL;
+            Bundle b = new Bundle();
+            b.putInt("mode",mode.ordinal());
+            summaryFragment.setArguments(b);
+
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, summaryFragment);
+            fragmentTransaction.commit();
+        }
+	    Resources res = getResources(); // Resource object to get Drawables
+/*
 	    TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
 	    
 	    Intent intent = new Intent().setClass(this, SummaryActivityAlphabetical.class);
@@ -57,6 +89,6 @@ public class AgileProjectActivity extends TabActivity {
 	    spec = tabHost.newTabSpec("Alerts").setIndicator("", res.getDrawable(R.drawable.ic_tab_alerts)).setContent(intent);
 	    tabHost.addTab(spec);
 
-	    tabHost.setCurrentTab(0);
+	    tabHost.setCurrentTab(0);*/
 	}
 }

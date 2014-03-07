3c1309cf05191a709745489de91d65473f2db50c
ronsch <ron.schoenberg@gmail.com>
2014-03-07 10:20:42 +0000

    SummaryActivity is refactored

--- AgileProject/src/com/StockTake/SummaryActivityAlphabetical.java
+++ AgileProject/src/com/StockTake/SummaryActivityAlphabetical.java
@@ -1,14 +1,17 @@
 package com.StockTake;
 
-
 import com.StockTake.StockManager.SortParameter;
 
 public class SummaryActivityAlphabetical extends SummaryActivity implements Param
 {
 	/** Called when the activity is first created. */
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see com.StockTake.SummaryActivity#getParam()
+	 * @return sortParameter for sorting alphabetically
+	 */
 	public StockManager.SortParameter getParam() {
 		return SortParameter.NAME ;
 	}
-
 }
